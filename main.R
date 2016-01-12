48dfeb69083cca81b492f9cc192529e17944ed93
BobHoutkooper <houtkooper.b@gmail.com>
2016-01-12 14:36:59 +0100

    Preprocessing done

--- main.R
+++ main.R
@@ -29,3 +29,18 @@ NDVI_NL_mean <- calc(NLModis, mean)
 
 # City data
 nlCity <- raster::getData('GADM',country='NLD', level=2)
+nlCity@data <- nlCity@data[!is.na(nlCity$NAME_2),]
+nlCity1 <- spTransform(nlCity, CRS(proj4string(NDVI_NL_Jan)))
+
+# Mask NDVI
+NDVI_Jan <- mask(NDVI_NL_Jan, nlCity1)
+NDVI_Dec <- mask(NDVI_NL_Dec, nlCity1)
+NDVI_Mean <- mask(NDVI_NL_mean, nlCity1)
+
+# Extract
+NDVI_Jan_city <-
+NDVI_Dec_city <-
+NDVI_mean_city <-
+
+
+
